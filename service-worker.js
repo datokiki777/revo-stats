bc862c6cb23a689bbd953475cfd0666199c1b18e
Davit <mamulashvili1985@gmail.com>
2026-04-26 14:18:52 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "revo-stats-shell-v1.9";
-const RUNTIME_CACHE = "revo-stats-runtime-v1.9";
+const CACHE = "revo-stats-shell-v2.0";
+const RUNTIME_CACHE = "revo-stats-runtime-v2.0";
 
 const CORE_ASSETS = [
   "./",
