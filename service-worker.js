716aa2beb12772e9b7a960e1bb46123cbea1600c
Davit <mamulashvili1985@gmail.com>
2026-04-26 15:10:42 +0200

    Update service worker cache version to v2.4

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "revo-stats-shell-v2.3";
-const RUNTIME_CACHE = "revo-stats-runtime-v2.3";
+const CACHE = "revo-stats-shell-v2.4";
+const RUNTIME_CACHE = "revo-stats-runtime-v2.4";
 
 const CORE_ASSETS = [
   "./",
